458cddbb7d1e0b17670f676d9ff1105a94232369
unknown <joe7.world@gmail.com>
2017-01-24 00:43:21 +0300

    Lesson1

---
+++ HomeWork1/src/geekbrains/java_level_one/hw1/Main.java
@@ -0,0 +1,26 @@
+package geekbrains.java_level_one.hw1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        public static int myfunc (int x, int y, int z, int w) {
+            return x +y + z + w;
+        }
+        int a = 774474543;
+        byte b = 55;
+        short c = 6000;
+        long d = 1829382372;
+        float e = 14.5f;
+        double f = 10.05;
+        boolean g = true;
+        char h = 's';
+        string i = "User";
+
+        int sum = myfunc(1, 2, 3, 4);
+
+
+    }
+
+
+}
---
+++ geekbrains.java_level_one.lesson_one/src/HelloPeople.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Jess on 23.01.2017.
+ */
+public class HelloPeople {
+    public static void main (String[] args){
+        int a = 10;
+        int b = 3;
+        int c = a % b;
+
+        System.out.println(c);
+    }
+}

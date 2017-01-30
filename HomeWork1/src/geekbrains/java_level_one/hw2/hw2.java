c6076c80679da79e935e4e7e99d340296d8424e3
unknown <joe7.world@gmail.com>
2017-01-30 16:09:25 +0300

    hw2

--- HomeWork1/src/geekbrains/java_level_one/hw2/hw2.java
+++ HomeWork1/src/geekbrains/java_level_one/hw2/hw2.java
@@ -5,26 +5,50 @@ import java.util.Arrays;
 
 public class hw2 {
 
-    private static void change_binary(int[] c_binary) {
-        for (int i = 0; i < c_binary.length; i++) {
-            c_binary[i] = (c_binary[i] == 0) ? 1 : 0;
+    private static void change_binary(int[] binary) {
+        for (int i = 0; i < binary.length; i++) {
+            binary[i] = (binary[i] == 0) ? 1 : 0;
         }
     }
 
     private static void fill_array8 (int[] f_array8) {
+        int b = 1;
         for (int i = 0; i < f_array8.length; i++) {
-            int j = 2; f_array8[i] = j; j = j + 3;
+            f_array8[i] = b; b+=3;
+        }
+    }
+
+    private static void multiply_two (int[] mas_multiply_two) {
+        for (int i = 0; i < mas_multiply_two.length; i++) {
+            if (mas_multiply_two[i] < 6) mas_multiply_two[i] *= 2;
+        }
+    }
+
+    private static void max_min (int[] max_min_mas) {
+        int max = max_min_mas[0];
+        int min = max_min_mas[0];
+        for (int i = 0; i < max_min_mas.length; i++) {
+            if(max < max_min_mas[i]) max = max_min_mas[i];
+            if(min > max_min_mas[i]) min = max_min_mas[i];
         }
     }
 
     public static void main(String[] args) {
 
-        int[] binary = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        change_binary(binary);
-        System.out.println(Arrays.toString(binary));
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        change_binary(arr);
+        System.out.println(Arrays.toString(arr));
 
         int[] array8 = new int[8];
         fill_array8(array8);
         System.out.println(Arrays.toString(array8));
+
+        int[] mas = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        multiply_two(mas);
+        System.out.println(Arrays.toString(mas));
+
+        int[] mas2 = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        max_min(mas2);
+        // System.out.println();
     }
 }

72f8b4e18321b356a98248bff9a5a04a0fedfcbc
Jess13 <joe7.world@gmail.com>
2017-02-28 21:32:45 +0300

    hw8.2

--- HomeWork1/src/geekbrains/java_level_one_lesson_eight/Test.java
+++ HomeWork1/src/geekbrains/java_level_one_lesson_eight/Test.java
@@ -1,6 +1,9 @@
 package geekbrains.java_level_one_lesson_eight;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by spo_admin4 on 21.02.2017.
@@ -17,6 +20,23 @@ public class Test {
             setTitle("Тестовое окошко!!!");
             setSize(300, 300);
             setLocation(300, 300);
+            JButton exitBut = new JButton("Выход");
+            exitBut.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+                }
+            });
+
+            Map map = new Map();
+
+            JPanel pBottom = new JPanel();
+            pBottom.setLayout(new GridLayout(1, 2));
+            pBottom.add(exitBut);
+
+            add(map, BorderLayout.CENTER);
+            add(pBottom, BorderLayout.SOUTH);
+
             setVisible(true);
         }
     }

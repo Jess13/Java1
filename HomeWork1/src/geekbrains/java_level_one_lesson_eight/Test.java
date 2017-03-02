a24f49ffdd7db3466e20dc68716269bb7562bd46
unknown <joe7.world@gmail.com>
2017-03-03 00:41:55 +0300

    hw8.3

--- HomeWork1/src/geekbrains/java_level_one_lesson_eight/Test.java
+++ HomeWork1/src/geekbrains/java_level_one_lesson_eight/Test.java
@@ -17,10 +17,20 @@ public class Test {
 
         GWindow() {
             setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            setTitle("Тестовое окошко!!!");
+            setTitle("Крестики-Нолики!!!");
             setSize(300, 300);
-            setLocation(300, 300);
-            JButton exitBut = new JButton("Выход");
+            setLocation(700,400);
+
+            JButton gameBut = new JButton("Играть");
+            gameBut.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    dispose();
+                    new GWindow();
+                    //startGame();
+                }
+            });
+            JButton exitBut = new JButton("Выйти");
             exitBut.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
@@ -28,16 +38,20 @@ public class Test {
                 }
             });
 
-            Map map = new Map();
+            //Map map = new Map();
 
             JPanel pBottom = new JPanel();
-            pBottom.setLayout(new GridLayout(1, 2));
+            pBottom.setLayout(new GridLayout(2, 1));
+            pBottom.add(gameBut);
             pBottom.add(exitBut);
 
-            add(map, BorderLayout.CENTER);
+            //add(map, BorderLayout.CENTER);
             add(pBottom, BorderLayout.SOUTH);
 
             setVisible(true);
         }
+    }
+    static void startGame() {
+        //
     }
 }
